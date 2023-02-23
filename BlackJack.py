850428aa1292f495d26cc9cf3303540480dbd466
Illko <illabegal@gmail.com>
2023-02-23 14:59:31 +0200

    Add 'Hand' class with english comments

--- BlackJack.py
+++ BlackJack.py
@@ -17,3 +17,31 @@ class Card(object):
 
     def __str__(self):
         return f"{self.rank}{self.suit}"
+
+
+class Hand(object):
+    def __init__(self, name):
+        self.name = name  # player name
+        self.cards = []  # emtpy hand
+
+    def add_card(self, card):  # add card on hand
+        self.cards.append(card)
+
+    def get_value(self):  # method getting the points
+        result = 0
+        aces = 0  # count of A in hand
+        for card in self.cards:
+            result += card.card_value()
+            if card.get_rank() == "A":  # if A on hand - aces ++
+                aces += 1
+        if result + aces * 10 <= 21:  # deciding T = 1 point ot T = 11 point
+            result += aces * 10
+        return result
+
+    def __str__(self):
+        text = f"{self.name}'s contains:\n"
+        for card in self.cards:
+            text += str(card) + " "
+        text += f"\nHand value: {self.get_value()}"
+        return text
+
